f0982b1f7ec2c3d7c8328ae6f4b5e214d62ddd36
russell f <russell.frame@machiningcloud.com>
2017-07-11 16:07:08 -0700

    Firebase: Advanced Topics - Joining

--- pepper/src/app/app.component.ts
+++ pepper/src/app/app.component.ts
@@ -1,52 +1,32 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2/database';
 import {Subscription} from 'rxjs/Subscription';
+import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/Observable';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  title = 'app';
+export class AppComponent implements OnInit {
   cuisines: FirebaseListObservable<any[]>;
-  restaurant: FirebaseObjectObservable<any>;
-  // private subscriptions: Array<Subscription> = [];
+  restaurants: Observable<any[]>;
 
   constructor(private db: AngularFireDatabase) {
   }
 
   ngOnInit(): void {
     this.cuisines = this.db.list('/cuisines');
-    this.restaurant = this.db.object('/restaurant');
-    // this.subscriptions.push(this.db.list('/cuisines').subscribe(x => this.cuisines = x));
-  }
-  add() {
-    this.cuisines.push({name: 'Asian', details: {description: '...'}});
-  }
-  update() {
-    //non destructive
-    // this.db.object('/restaurant').update({
-    //   name: 'New Name2',
-    //   rating: 5
-    // });
-    //Destructive updates
-    //overwrite existing object
-    //will also create if not there
-    // this.db.object('/restaurant').set({
-    //   name: 'New Name',
-    //   rating: 5
-    // });
-    // favorites/{userId}/{restaurantId}
-    // this.db.object('favorites/1/10').set(true);
-    this.db.object('/favorites/1/10').set(null);
-  }
-  remove() {
-    this.db.object('/restaurant').remove()
-      .then(x => console.log('SUCCESS'))
-      .catch(error => console.log('Error', error));
-  }
-  ngOnDestroy(): void {
-    // this.subscriptions.forEach(x => x.unsubscribe());
+    this.restaurants = this.db.list('/restaurants')
+      .map(restaurants => {
+        console.log('Before Map', restaurants);
+        restaurants.map(x => {
+          x.cuisineType = this.db.object(`/cuisines/${x.cuisine}`);
+        });
+        console.log('AfterMap', restaurants);
+        return restaurants;
+      });
   }
 }
 
