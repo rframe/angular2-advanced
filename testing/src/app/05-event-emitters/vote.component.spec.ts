683560fd8d02c503e04a5831c3f8436ecd12c161
russell f <russell.frame@machiningcloud.com>
2017-07-24 11:18:36 -0700

    Unit Testing Angular Apps: Working with Event Emitters

--- testing/src/app/05-event-emitters/vote.component.spec.ts
+++ testing/src/app/05-event-emitters/vote.component.spec.ts
@@ -1,12 +1,22 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  var component: VoteComponent; 
+  var component: VoteComponent;
 
   beforeEach(() => {
     component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should raise vote changed event when updated', () => {
+    let totalVotes = null;
+
+    component.voteChanged.subscribe(tv => {
+      totalVotes = tv;
+    });
+
+    component.upVote();
+
+    // expect(totalVotes).not.toBeNull(); // To vague
+    expect(totalVotes).toBe(1);
   });
 });
