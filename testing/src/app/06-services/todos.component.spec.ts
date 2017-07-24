0d32ffec29496105d9bf2a01c653187e29e0fb18
russell f <russell.frame@machiningcloud.com>
2017-07-24 11:19:00 -0700

    Unit Testing Angular Apps: Working with Spies

--- testing/src/app/06-services/todos.component.spec.ts
+++ testing/src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,38 @@
-import { TodosComponent } from './todos.component'; 
-import { TodoService } from './todo.service'; 
+import { TodosComponent } from './todos.component';
+import { TodoService } from './todo.service';
+import {of} from 'rxjs/observable/of';
+import 'rxjs/add/observable/from';
+import {Observable} from 'rxjs/Observable';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
+  let service: TodoService;
 
   beforeEach(() => {
+    service = new TodoService(null);
+
+    // Other option is to stub
+    component = new TodosComponent(service);
   });
 
-  it('', () => {
+  it('should set todos property with the items returned from the server', () => {
+    let todos = [1,2,3];
+    spyOn(service, 'getTodos').and.callFake(() => {
+      // return of([])
+      return Observable.from([todos])
+      // return Observable.from([[
+      //   {id: 1, title: 'a'},
+      //   {id: 2, title: 'b'},
+      //   {id: 3, title: 'c'}
+      // ]]);
+    });
+
+    component.ngOnInit();
+
+    // To General
+    // expect(component.todos.length).toBeGreaterThan(0);
+    // To General
+    // expect(component.todos.length).toBe(3);
+    expect(component.todos).toBe(todos);
   });
 });
