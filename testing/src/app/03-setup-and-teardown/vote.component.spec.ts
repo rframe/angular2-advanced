24e3a4a68e6e2c2e50cad92328bade0ab0f7f393
russell f <russell.frame@machiningcloud.com>
2017-07-24 10:54:03 -0700

    Unit Testing Angular Apps: Set up an tear down

--- testing/src/app/03-setup-and-teardown/vote.component.spec.ts
+++ testing/src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,38 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
+// State change pattern
 describe('VoteComponent', () => {
-  it('', () => {
+  let component: VoteComponent;
+
+  // Before each of the test n times
+  beforeEach(() => {
+    // Set Up
+    component = new VoteComponent();
+  });
+  // After each of the test n times
+  afterEach(() =>{
+    // Tear down
   });
 
-  it('', () => {
+  //Before all tests once
+  beforeAll(() => {});
+  //After all tests once
+  afterAll(() => {});
+
+  it('should increment the totalVotes when upvted', () => {
+    // Arrange, create system under test
+    // let component = new VoteComponent();
+
+    // ACT
+    component.upVote();
+
+    // Assert
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('should decrement the totalVotes when downvted', () => {
+    component.downVote();
+
+    expect(component.totalVotes).toBe(-1);
   });
 });
