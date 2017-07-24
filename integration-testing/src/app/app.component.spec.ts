846f4ffda89d478485f65fbf047bc944ae7b51a9
russell f <russell.frame@machiningcloud.com>
2017-07-24 15:05:07 -0700

    Integration Testing Angular Apps: Shallow Component Tests

--- integration-testing/src/app/app.component.spec.ts
+++ integration-testing/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing'; 
+import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterOutlet} from '@angular/router';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {NavComponent} from './nav/nav.component';
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -12,7 +14,10 @@ describe('AppComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppComponent ]
+      imports: [ RouterTestingModule.withRoutes([]) ],
+      declarations: [ AppComponent, NavComponent ]
+      // declarations: [ AppComponent ],
+      // schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -20,6 +25,11 @@ describe('AppComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges(); 
+    fixture.detectChanges();
+  });
+
+  it('should have router outlet', () => {
+    const de = fixture.debugElement.query(By.directive(RouterOutlet));
+    expect(de).not.toBeNull();
   });
 });
